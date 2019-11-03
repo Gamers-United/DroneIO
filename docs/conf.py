e4d22bda77d675f858b982b7ffd726f0bd791757
Macauley Lim <xmachd@gmail.com>
2019-11-03 17:00:19 +1100

    domain indices

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-
+html_domain_indices = True
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
