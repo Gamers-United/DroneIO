90fd698412c96dc1f28b60b23416435461aca3df
Macauley Lim <xmachd@gmail.com>
2019-11-06 23:12:51 +1100

    doc update

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ release = '0.2'
 extensions = [
     "sphinx.ext.autodoc",
     'sphinx_rtd_theme',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
